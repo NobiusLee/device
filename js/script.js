426468ad58d9b6377b85ef26598416c28e0a16c8
NobiusLee <forestgun186@gmail.com>
2021-01-12 17:31:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+
+/*
 $(document).ready(function () {
     $('.header__menu-burger').click(function (event) {
         $('.header__menu-burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
+    });
+});
+*/
+
+$(document).ready(function () {
+    $('.header__dropdown-directory-textus').click(function (event) {
+        $('.header__last-line, .header__dropdown-page').toggleClass('dropdown-page');
+        //$('body').toggleClass('lock');
     });
 });
