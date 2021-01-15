eb11af4d534c4177e71df53727ea0b90168122ca
NobiusLee <forestgun186@gmail.com>
2021-01-15 05:34:57 +0300

    dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
-/*
+
 $(document).ready(function () {
     $('.header__menu-burger').click(function (event) {
         $('.header__menu-burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
-*/
+
 
 $(document).ready(function () {
     $('.header__dropdown-directory-textus').click(function (event) {
